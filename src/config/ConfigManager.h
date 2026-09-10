f0c54ffdabcb6b6d665e22af4629321ff4f2f675
vitalya482-glitch <vitalya482@gmail.com>
2026-09-10 11:03:54 +0500

    Add sampling penalty settings to model profiles

--- src/config/ConfigManager.h
+++ src/config/ConfigManager.h
@@ -9,6 +9,7 @@ struct Profile {
     std::filesystem::path model;
     int context = 32768, parallel = 1, gpuLayers = 999, cpuMoe = 27;
     double temperature = 0.3;
+    double presencePenalty = 0.0, repeatPenalty = 1.0, frequencyPenalty = 0.0;
     std::string kvK = "q8_0", kvV = "q8_0", tools = "all", toolsRuntime = "docker:ai-cpp-sandbox";
     bool flashAttention = true;
 };
